445b1a8ceb258efa9fec12460efd18002228e8b1
Li Haoyi <haoyi@dropbox.com>
2014-04-22 23:29:07 -0700

    lots of changes

--- shared/main/scala/upickle/Types.scala
+++ shared/main/scala/upickle/Types.scala
@@ -4,6 +4,5 @@ trait Writer[T]{val write: T => Js.Value}
 trait Reader[T]{val read: Js.Value => T}
 class WriterCls[T](val write: T => Js.Value) extends Writer[T]
 class ReaderCls[T](val read: Js.Value => T) extends Reader[T]
-
 class ReadWriter[T](val write: T => Js.Value, val read: Js.Value => T) extends Writer[T] with Reader[T]
 
