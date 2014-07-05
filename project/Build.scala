46c6201faeb7c8e03b5519dc48ecbcb7dd250fba
Li Haoyi <haoyi@dropbox.com>
2014-07-04 22:38:05 -0700

    0.1.3 4 reals

--- project/Build.scala
+++ project/Build.scala
@@ -10,7 +10,7 @@ object Build extends sbt.Build{
   val cross = new utest.jsrunner.JsCrossBuild(
     organization := "com.lihaoyi",
 
-    version := "0.1.2",
+    version := "0.1.3",
     scalaVersion := "2.11.0",
     name := "upickle",
 
--- project/build.sbt
+++ project/build.sbt
@@ -1,3 +1,3 @@
 addSbtPlugin("org.scala-lang.modules.scalajs" % "scalajs-sbt-plugin" % "0.5.0")
 
-addSbtPlugin("com.lihaoyi" % "utest-js-plugin" % "0.1.6")
+addSbtPlugin("com.lihaoyi" % "utest-js-plugin" % "0.1.7")
