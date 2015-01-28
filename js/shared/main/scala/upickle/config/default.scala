c5bd972855378b2593d42181ca700049a397e6ac
Vladimir Matveev <vladimir.matweev@gmail.com>
2015-01-28 17:20:45 +0300

    Fixed name conflict of classes on case-insensitive file systems

--- js/shared/main/scala/upickle/config/default.scala
+++ js/shared/main/scala/upickle/config/default.scala
@@ -2,8 +2,8 @@ package upickle.config
 
 import upickle.Annotator
 
-trait Default extends Config {
+trait DefaultConfig extends Config {
   override implicit val annotator = Annotator.keyAnnotator("$variant")
 }
 
-object default extends Default
+object default extends DefaultConfig
--- js/shared/main/scala/upickle/config/old.scala
+++ js/shared/main/scala/upickle/config/old.scala
@@ -2,8 +2,8 @@ package upickle.config
 
 import upickle.Annotator
 
-trait Old extends Config {
+trait OldConfig extends Config {
   override implicit val annotator = Annotator.arrayAnnotator
 }
 
-object old extends Old
+object old extends OldConfig
