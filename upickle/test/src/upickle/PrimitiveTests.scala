563eaaa8b2c1d9ddaa2db54c7785eb4b5843edcc
Anatolii Kmetiuk <anatoliykmetyuk@gmail.com>
2020-06-04 17:53:46 +0200

    PrimitiveTests work. Symbols support dropped
    Symbols are dropped from Scala 3

--- upickle/test/src/upickle/PrimitiveTests.scala
+++ upickle/test/src/upickle/PrimitiveTests.scala
@@ -31,11 +31,6 @@ object PrimitiveTests extends TestSuite {
         }
       }
     }
-    test("Symbol"){
-      test("plain") - rw("i_am_a_cow", """ "i_am_a_cow" """)(upickle.default.SymbolReader, upickle.default.SymbolWriter)
-      test("unicode") - rw("叉烧包", """ "叉烧包" """)
-      test("null") - rw(null: Symbol, "null")
-    }
     test("Long"){
       test("small") - rw(1: Long, """ "1" """)
       test("med") - rw(125123: Long, """ "125123" """)
