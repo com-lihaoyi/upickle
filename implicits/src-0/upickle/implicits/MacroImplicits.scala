eefecdc78631c0fb3351352d6e59735c9c526782
Jakob Odersky <jakob@inpher.io>
2020-08-06 12:50:14 +0200

    Add macroRW to MacroImplicits

--- implicits/src-0/upickle/implicits/MacroImplicits.scala
+++ implicits/src-0/upickle/implicits/MacroImplicits.scala
@@ -1,8 +1,21 @@
 package upickle.implicits
 
+import deriving.Mirror
+import scala.reflect.ClassTag
+
 // This trait is needed only for
 // JsReadWriters and MsgReadWriters
 // to take priority over macro implicits.
 // TODO come up with a better design without
 // this dummy trait
-trait MacroImplicits
+trait MacroImplicits extends Readers with Writers with upickle.core.Annotator:
+  this: upickle.core.Types =>
+
+  inline def macroRW[T: ClassTag](using Mirror.Of[T]): ReadWriter[T] =
+    ReadWriter.join(
+      macroR[T],
+      macroW[T]
+    )
+  end macroRW
+
+end MacroImplicits
