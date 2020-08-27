da56e802d43a2d8878048444c40cf4b6a9c9b1c2
Jakob Odersky <jakob@inpher.io>
2020-08-27 14:40:24 +0200

    Deprioritize JsValue rws through while keeping bin compat

--- upickle/src/upickle/JsReadWriters.scala
+++ upickle/src/upickle/JsReadWriters.scala
@@ -4,7 +4,7 @@ import upickle.core.Visitor
 import upickle.implicits.MacroImplicits
 
 
-trait JsReadWriters extends upickle.core.Types with MacroImplicits {
+trait JsReadWriters extends upickle.core.Types with MacroImplicits with LowPriReadWriters {
 
   implicit def JsObjR: Reader[ujson.Obj] = JsValueR.narrow[ujson.Obj]
   implicit def JsArrR: Reader[ujson.Arr] = JsValueR.narrow[ujson.Arr]
@@ -15,7 +15,6 @@ trait JsReadWriters extends upickle.core.Types with MacroImplicits {
   implicit def JsFalseR: Reader[ujson.False.type] = JsValueR.narrow[ujson.False.type]
   implicit def JsNullR: Reader[ujson.Null.type] = JsValueR.narrow[ujson.Null.type]
 
-
   implicit def JsObjW: Writer[ujson.Obj] = JsValueW.narrow[ujson.Obj]
   implicit def JsArrW: Writer[ujson.Arr] = JsValueW.narrow[ujson.Arr]
   implicit def JsStrW: Writer[ujson.Str] = JsValueW.narrow[ujson.Str]
@@ -25,14 +24,13 @@ trait JsReadWriters extends upickle.core.Types with MacroImplicits {
   implicit def JsFalseW: Writer[ujson.False.type] = JsValueW.narrow[ujson.False.type]
   implicit def JsNullW: Writer[ujson.Null.type] = JsValueW.narrow[ujson.Null.type]
 
-  // This trait has no instance. Its only purpose is to deprioritize implicit
-  // lookup of JsValueR and JsValueW over all other instances. This is not
-  // required in Scala 2, but is required in Dotty to avoid implicit ambiguities
-  // when summoning readers and writers in certain circumstances.
-  trait ImplicitDummy
+}
 
-  implicit def JsValueR(implicit dummy: ImplicitDummy = null): Reader[ujson.Value] = new Reader.Delegate(ujson.Value)
-  implicit def JsValueW(implicit dummy: ImplicitDummy = null): Writer[ujson.Value] = new Writer[ujson.Value] {
+trait LowPriReadWriters { self: JsReadWriters =>
+
+  implicit def JsValueR: Reader[ujson.Value] = new Reader.Delegate(ujson.Value)
+  implicit def JsValueW: Writer[ujson.Value] = new Writer[ujson.Value] {
     def write0[R](out: Visitor[_, R], v: ujson.Value): R = ujson.transform(v, out)
   }
+
 }
