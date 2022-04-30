b24d7d67b25b2390f0c4f4c5f8b972bf691a240d
Igal Tabachnik <hmemcpy@gmail.com>
2022-04-30 14:11:36 +0300

    Correct url to the documentation about manual sealed trait pickling

--- implicits/src-2/upickle/implicits/internal/Macros.scala
+++ implicits/src-2/upickle/implicits/internal/Macros.scala
@@ -121,7 +121,7 @@ object Macros {
           s"The referenced trait [[${clsSymbol.name}]] does not have any sub-classes. This may " +
             "happen due to a limitation of scalac (SI-7046). To work around this, " +
             "try manually specifying the sealed trait picklers as described in " +
-            "http://www.lihaoyi.com/upickle/#ManualSealedTraitPicklers"
+            "https://com-lihaoyi.github.io/upickle/#ManualSealedTraitPicklers"
         fail(tpe, msg)
       }else{
         val subTypes = fleshedOutSubtypes(tpe).toSeq.sortBy(_.typeSymbol.fullName)
