dbc36311a67e22533edef4d82c5275be57e2bbb2
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2023-03-10 09:30:28 +0100

    Fix compilation for Scala 2.12

--- core/src-2.12/upickle/core/compat/LinkedHashMapCompat.scala
+++ core/src-2.12/upickle/core/compat/LinkedHashMapCompat.scala
@@ -1,6 +1,6 @@
 package upickle.core.compat
 
-import upickle.core.compat.Factory
+import upickle.core.LinkedHashMap
 
 import scala.collection.mutable
 
@@ -10,9 +10,18 @@ trait LinkedHashMapCompat[K, V] {
   override def +=(kv: (K, V)): this.type = addOne(kv)
 }
 object LinkedHashMapCompat {
-  def factory[K, V]: Factory[(K, V), mutable.Map[K, V]] =
-    new Factory[(K, V), mutable.Map[K, V]] {
-      def apply() = LinkedHashMap()
+  def factory[K, V]: Factory[(K, V), LinkedHashMap[K, V]] =
+    new Factory[(K, V), LinkedHashMap[K, V]] {
+      def apply() = new mutable.Builder[(K, V), LinkedHashMap[K, V]] {
+        private val map = LinkedHashMap[K, V]()
+
+        def +=(elem: (K, V)): this.type = {
+          map.addOne(elem)
+          this
+        }
+        def clear(): Unit = map.clear()
+        def result(): LinkedHashMap[K, V] = map
+      }
       def apply(it: Nothing) = ???
     }
 }
