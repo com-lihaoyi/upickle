6e946e8bb17083a86cda0d931653ae6bfd3b2644
Li Haoyi <haoyi.sg@gmail.com>
2024-02-03 21:22:58 +0800

    .

--- upickle/core/src-2.12/upickle/core/compat/SortInPlace.scala
+++ upickle/core/src-2.12/upickle/core/compat/SortInPlace.scala
@@ -1,7 +1,7 @@
 package upickle.core.compat
 
 object SortInPlace {
-  def apply[T, B](t: collection.mutable.ArrayBuffer[T])(f: T => B): Unit = {
+  def apply[T, B: Ordering](t: collection.mutable.ArrayBuffer[T])(f: T => B): Unit = {
     val sorted = t.sortBy(f)
     t.clear()
     t.appendAll(sorted)
